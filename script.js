2d7d49dc97891a7c4b7a60f755834fc3ccd6c7c5
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-09-17 12:47:25 +0400

    change

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const role = document.querySelector(".role");
 
 const testimonials = [
   {
-    name: "Aram Myles",
+    name: "Dina Myles",
     position: "Marketing",
     photo:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6",
